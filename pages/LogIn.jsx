41748230980dd5548c0ad8b2a3c7ab707150f5cd
Tamagochi02 <mirytamara@gmail.com>
2022-03-14 09:21:15 -0600

    Creacion de Login

---
+++ pages/LogIn.jsx
@@ -0,0 +1,11 @@
+const login = () => {
+    return (
+      <div className="h-screen w-screen flex">
+        
+        <div className="w-50 h-screen bg-gray-100"></div>
+        <div></div>
+      </div>
+    );
+  };
+  
+  export default login;
