bbdc4a2db07ca2052f956c71adc76e2a6adc31f2
JazOki <castellanossilvia537@gmail.com>
2022-03-14 08:53:07 -0600

    cambios1

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,12 @@
 const Layout = ({children, nombre}) => {
     return (
       <div className="h-screen w-screen flex">
-        <div className="w-36 h-screen bg-gray-100"></div>
+        <div className="w-36 h-screen bg-gray-100">
+          <a className="hover:bg-blue-400 w-full flex items-center justify-center">
+            <span className="material-icons">schedule</span>
+            <span className="flex items-center">recents</span>
+          </a>
+        </div>
         <div className="w-full flex flex-col">
           <div className="h-16 bg-gray-100 text-right ">{nombre}</div>
           <div className="h-full p-5">
--- pages/prueba.jsx
+++ pages/prueba.jsx
@@ -3,6 +3,7 @@ import Layout from "../components/Layout";
 const Prueba = () => {
   return < Layout nombre="Jaz">
   Hola components
+  <span className="material-icons text-red-500">polymer</span>   
   </Layout>
 };
 
