4a3d85ed5c5596da3d6f2909a882b4cd3bcdf35a
JazOki <castellanossilvia537@gmail.com>
2022-03-15 10:05:56 -0600

    Modificciones

--- pages/LogIn.jsx
+++ pages/LogIn.jsx
@@ -3,8 +3,7 @@ const LogIn = () => {
         <div id="contenedorPrincipal" className="h-screen w-screen flex">
             <div id="contenedorIzquierdo" className="w-2/3 h-screen bg-gray-800">
                 <div className="font-serif text-center text-4xl pt-7 text-white">  ¡Bienvenido! </div>
-                <span class="material-icons-outlined text-white items-center">
-                    account_circle
+                <span class="material-icons-outlined text-white items-center"> account_circle
                 </span>
                 <div className="font-serif text-left text-xl pl-10 text-white "> Correo institucional: </div>
                 <div className="pl-10 w-16"> <input type="text"></input> </div>
