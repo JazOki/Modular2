f7784bb7d106a72dba2c8a287b24142c7c8f4c3e
Tamagochi02 <mirytamara@gmail.com>
2022-03-15 12:10:00 -0600

    Modificacion

--- pages/LogIn.jsx
+++ pages/LogIn.jsx
@@ -2,15 +2,15 @@ const LogIn = () => {
     return (
         <div id="contenedorPrincipal" className="h-screen w-screen flex">
             <div id="contenedorIzquierdo" className="w-2/3 h-screen bg-gray-800">
-                <div className="font-serif text-center text-4xl pt-7 text-white">  ¡Bienvenido! </div>
+                <div className="font-serif text-center text-4xl pt-10 text-white">  ¡Bienvenido! </div>
                 <span class=" text-2xl justify-center material-icons-outlined text-white">
                     account_circle
                 </span>
                 <p className="font-serif text-left text-xl pl-24 text-white"> Correo institucional: </p>
                 <div className="pl-24 pt-2"><input type="text" className="w-48 rounded"></input> </div>
-                <p className="pt-1 font-sans text-white absolute left-60 top-32"> @universidad-une.com </p>
+                <p className="pt-4 font-sans text-white absolute left-72 top-32"> @universidad-une.com </p>
                 <p className="font-serif text-left text-xl pl-24 pt-5 text-white top-56"> Contraseña: </p>
-                <div className="pl-10 pt-2"><input type="password" className="w-80 rounded"></input> </div>
+                <div className="pl-24 pt-2"><input type="password" className="w-80 rounded"></input> </div>
                 <span class="material-icons-outlined text-white">
                     visibility
                 </span>
