907a501035d29e041a2e0a1a4343ad0a3750f66d
andizki <andy190495mrtz@gmail.com>
2022-03-15 14:27:25 -0600

    modificado

--- pages/registroEtapa2.jsx
+++ pages/registroEtapa2.jsx
@@ -6,10 +6,25 @@ const registroEtapa2 = () => {
         <div className="w-full flex flex-col bg-gray-800">
           <div className="h-16 bg-gray-100 text-right "></div>
           <div className="font-serif text-left text-xl text-left pl-10 text-white ">Titulo<input type="text"></input></div>
-          <div className="text-left text-xl text-right  pl-10 text-white">Modulo <input type="select"></input></div> 
-          <div className="text-left text-xl pl-10 text-white">Descripcion <input type="text-medium"></input></div>
-          <div className="text-left text-xl pl-10 text-white">Objetivo General <input type="text"></input></div>
-           
+          <div id="moduloBox" className="font-serif text-xl text-left pl-10 text-white pl-24 pt-2">Modulo
+                    <select name="select" className="font-serif w-48 rounded bg-white text-black w-80">
+                        <option value="nothing"> Selecciona un modulo... </option>
+                        <option value="etapa1"> Modulo 1 </option>
+                        <option value="etapa2"> Modulo 2 </option>
+                        <option value="etapa3"> Modulo 3 </option>
+                    </select>
+                </div>
+          <div className="font-serif text-left text-xl pl-10 text-white">Descripcion <input type="text-medium"></input></div>
+          <div className="font-serif rounded text-left text-xl pl-10 text-white">Objetivo General <input type="text"></input></div>
+          <div className="font-serif rounded text-left text-xl pl-10 text-white">Alcance <input type="text"></input></div>
+          <div className="font-serif rounded text-left text-xl pl-10 text-white">Herramientas <input type="text"></input></div>
+          <div id="moduloBox" className="font-serif text-xl text-left pl-10 text-white pl-24 pt-2">Asesor
+                    <select name="select" className="font-serif w-48 rounded bg-white text-black w-80">
+                        <option value="nothing"> Selecciona un Asesor... </option>
+                        <option value="etapa1"> Juan Antonio Carbajal Martinez </option>
+                        <option value="etapa2"> Carlos Ivan Medina Lopez </option>
+                    </select>
+                </div>
           </div>
         </div>
     );
