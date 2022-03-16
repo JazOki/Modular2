07fefb268e131df651db6517b76e4337f56b1a47
JazOki <castellanossilvia537@gmail.com>
2022-03-16 08:41:25 -0600

    fix Layout add SidebarButton

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,102 +1,53 @@
-const Layout = ({children, nombre, matricula, codigo}) => {
-    return (
-      <div className="w-full flex flex-col">
-        {
-          //parte de izquiero
-      }
-      <div className="bg-blue-900 text-right h-14 w-full flex items-center justify-center">
-        <span className="text-white material-icons">account_circle </span>
-        <span className="text-white font-serif  flex items-center"> {nombre}</span>
-        <p className="style=margin-left: 10px text-white h-11 font-serif flex text-center items-start justify-start">Alumno</p>
-      </div>
-      <div className="h-full p-0">{children}</div>
-      {
-          //parte de izquierdo
-      }
-      <div className="h-screen w-screen">
-      <div className="w-64 h-screen bg-gray-800">
-      <span className="text-white flex justify-center material-icons text-9xl">account_circle</span>
-        {
-          //manda llamar la variable matricula
-        }
-      <p className="text-white h-11 font-serif flex text-center items-center">Matrícula: {matricula}</p>
-      <p className="text-white h-11 font-serif flex items-center">Código: {codigo}</p>
-      <p className="text-blue-500 justify-center border-b border-blue-500 flex items-center">Documentación</p>
-      <button className="hover:bg-blue-400 h-14 w-full flex items-center justify-center">
-        <span className="text-white material-icons">description</span>
-        <span className="text-white font-serif  flex items-center">Registrar</span>
-      </button>
-      <button className="hover:bg-blue-400  h-14 w-full flex items-center justify-center">
-        <span className="text-white material-icons">note_alt</span>
-        <span className="text-white font-serif flex items-center">Modificaciones</span>
-      </button>
-      <button className="hover:bg-blue-400 h-14 w-full flex items-center justify-center">
-        <span className="text-white material-icons">source</span>
-        <span className="text-white font-serif flex items-center">Consulta</span>
-      </button>
-      <button className="hover:bg-blue-400 h-14 w-full flex items-center justify-center">
-        <span className="text-white material-icons">find_in_page</span>
-        <span className="text-white font-serif flex items-center">Observaciones</span>
-      </button>
-      <p className="text-blue-500 justify-center border-b border-blue-500 flex items-center">Configuraciones</p>
-      <button className="hover:bg-blue-400 h-12 w-full flex items-center justify-center">
-        <span className="text-white material-icons">person</span>
-        <span className="text-white font-serif flex items-center">Perfil</span>
-      </button>
-      <button className="hover:bg-blue-400 h-12 w-full flex items-center justify-center">
-        <span className="text-white material-icons">exit_to_app</span>
-        <span className="text-white font-serif flex items-center">Salir</span>
-      </button>
-        </div>
-      </div>
-      </div>
-     /* 
-      <div className="h-screen w-screen flex">
-        <div className="w-60 h-screen bg-gray-800">
-        <span className="text-white flex justify-center material-icons text-9xl">account_circle</span>
-        {
-          //manda llamar la variable matricula
-        }
-        <p className="text-white h-11 font-serif flex text-center items-center">Matrícula: {matricula}</p>
-        <p className="text-white h-11 font-serif flex items-center">Código: {codigo}</p>
-        <p className="text-blue-500 justify-center border-b border-blue-500 flex items-center">Documentación</p>
-          <button className="hover:bg-blue-400 h-14 w-full flex items-center justify-center">
-            <span className="text-white material-icons">description</span>
-            <span className="text-white font-serif  flex items-center">Registrar</span>
-          </button>
-          <button className="hover:bg-blue-400  h-14 w-full flex items-center justify-center">
-            <span className="text-white material-icons">note_alt</span>
-            <span className="text-white font-serif flex items-center">Modificaciones</span>
-          </button>
-          <button className="hover:bg-blue-400 h-14 w-full flex items-center justify-center">
-            <span className="text-white material-icons">source</span>
-            <span className="text-white font-serif flex items-center">Consulta</span>
-          </button>
-          <button className="hover:bg-blue-400 h-14 w-full flex items-center justify-center">
-            <span className="text-white material-icons">find_in_page</span>
-            <span className="text-white font-serif flex items-center">Observaciones</span>
-          </button>
-          <p className="text-blue-500 justify-center border-b border-blue-500 flex items-center">Configuraciones</p>
-          <button className="hover:bg-blue-400 h-12 w-full flex items-center justify-center">
-            <span className="text-white material-icons">person</span>
-            <span className="text-white font-serif flex items-center">Perfil</span>
-          </button>
-          <button className="hover:bg-blue-400 h-12 w-full flex items-center justify-center">
-            <span className="text-white material-icons">exit_to_app</span>
-            <span className="text-white font-serif flex items-center">Salir</span>
-          </button>
-        {//Div de la parte de arriba
-        }
-        </div>
-        <div className="w-full flex flex-col">
-          <div className="h-16 bg-gray-800 text-right ">{nombre}</div>
-          <div className="h-full p-5">
-            {children}
+import SidebarButton from "./SidebarButton";
+
+const Layout = ({ children, nombre, matricula, codigo }) => {
+  return (
+    <div className="flex flex-col w-screen h-screen">
+      <header className="px-5 bg-blue-900 text-right h-12 flex items-center justify-between">
+        <p className="ml-5 text-white font-serif flex text-center items-start justify-start">
+          Alumno
+        </p>
+        <p className="flex">
+          <span className="text-white mr-2 material-icons">
+            account_circle{" "}
+          </span>
+          <span className="text-white font-serif  flex items-center">
+            {" "}
+            {nombre}
+          </span>
+        </p>
+      </header>
+
+      <div className="flex flex-grow">
+        <aside className="w-64 bg-gray-800">
+          <div className="flex flex-col items-center">
+            <span className="text-white flex justify-center material-icons text-9xl">
+              account_circle
+            </span>
+            <p className="text-white font-serif flex items-center">
+              Matrícula: {matricula}
+            </p>
+            <p className="text-white font-serif flex items-center">
+              Código: {codigo}
+            </p>
           </div>
-        </div>
+          <p className="text-blue-500 justify-center border-b border-blue-500 flex items-center">
+            Documentación
+          </p>
+          <SidebarButton nombre="Registrar" icono="description" />
+          <SidebarButton nombre="Modificaciones" icono="note_alt" />
+          <SidebarButton nombre="Consulta" icono="source" />
+          <SidebarButton nombre="Observaciones" icono="find_in_page" />
+          <p className="text-blue-500 justify-center border-b border-blue-500 flex items-center">
+            Configuraciones
+          </p>
+          <SidebarButton nombre="Perfil" icono="person" />
+          <SidebarButton nombre="Salir" icono="exit_to_app" />
+        </aside>
+        <div className="flex-grow ">{children}</div>
       </div>
-      */
-    );
-  };
-  
-  export default Layout;
+    </div>
+  );
+};
+
+export default Layout;
