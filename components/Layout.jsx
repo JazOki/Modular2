b498b168720d1e54033bc86cdf53931cb22384d7
JazOki <castellanossilvia537@gmail.com>
2022-03-16 09:25:19 -0600

    update  login update index.js

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,7 +4,7 @@ const Layout = ({ children, nombre, matricula, codigo }) => {
   return (
     <div className="flex flex-col w-screen h-screen">
       <header className="px-5 bg-blue-900 text-right h-12 flex items-center justify-between">
-        <p className="ml-5 text-white font-serif flex text-center items-start justify-start">
+        <p className="ml-5 text-white font-serif flex text-center items-start px-14">
           Alumno
         </p>
         <p className="flex">
@@ -19,7 +19,7 @@ const Layout = ({ children, nombre, matricula, codigo }) => {
       </header>
 
       <div className="flex flex-grow">
-        <aside className="w-64 bg-gray-800">
+        <aside className="w-64 flex flex-col justify-evenly bg-gray-800">
           <div className="flex flex-col items-center">
             <span className="text-white flex justify-center material-icons text-9xl">
               account_circle
@@ -31,18 +31,22 @@ const Layout = ({ children, nombre, matricula, codigo }) => {
               Código: {codigo}
             </p>
           </div>
-          <p className="text-blue-500 justify-center border-b border-blue-500 flex items-center">
-            Documentación
-          </p>
-          <SidebarButton nombre="Registrar" icono="description" />
-          <SidebarButton nombre="Modificaciones" icono="note_alt" />
-          <SidebarButton nombre="Consulta" icono="source" />
-          <SidebarButton nombre="Observaciones" icono="find_in_page" />
-          <p className="text-blue-500 justify-center border-b border-blue-500 flex items-center">
-            Configuraciones
-          </p>
-          <SidebarButton nombre="Perfil" icono="person" />
-          <SidebarButton nombre="Salir" icono="exit_to_app" />
+          <div>
+            <p className="text-blue-500 justify-center border-b border-blue-500 flex items-center">
+              Documentación
+            </p>
+            <SidebarButton nombre="Registrar" icono="description" />
+            <SidebarButton nombre="Modificaciones" icono="note_alt" />
+            <SidebarButton nombre="Consulta" icono="source" />
+            <SidebarButton nombre="Observaciones" icono="find_in_page" />
+          </div>
+          <div>
+            <p className="text-blue-500 justify-center border-b border-blue-500 flex items-center">
+              Configuraciones
+            </p>
+            <SidebarButton nombre="Perfil" icono="person" />
+            <SidebarButton nombre="Salir" icono="exit_to_app" />
+          </div>
         </aside>
         <div className="flex-grow ">{children}</div>
       </div>
