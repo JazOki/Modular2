e7a88ebf58959a6b08f13cc424932008aeefc4de
andizki <andy190495mrtz@gmail.com>
2022-03-17 13:35:16 -0600

    modificacion consultarEtapas, modificacionEtapas

--- pages/modificacionEtapas.jsx
+++ pages/modificacionEtapas.jsx
@@ -7,18 +7,18 @@ const modificacionEtapas = () => {
                 <div className="h-16 bg-gray-100 text-right "></div>
                 <div className="font-serif w-2/6 h-80 text-left text-xl pl-14 pt-7 bg-gray-600 text-white">Escribre el nombre del proyecto:
                     <input type="text" className=" left pl-20 pt-4 w-62 h-9"></input>
-                    <div className="font-serif  text-left text-xl pl-0 pt-5  text-white">Elige el archivo PDF que deseas subir:
+                    <p className="font-serif  text-left text-xl pl-0 pt-5  text-white">Elige el archivo PDF que deseas subir:
                         <input type="text" className=" left pl-15 pt-4 w-30 h-9"></input> <button className="font-serif rounded w-30 h-12 text-2xl transition bg-gray-600 hover:-translate-y-1 hover:scale-110 hover:bg-gray-600 duration-150">
                             <span className="material-icons-outlined text-4xl">
                                 unarchive
                             </span>
-                        </button></div>
-                    <div><button className="font-serif rounded w-32 h-12 transition bg-gray-600 hover:-translate-y-1 hover:scale-110 hover:bg-gray-600 duration-150">
+                        </button></p>
+                    <p><button className="font-serif rounded w-32 h-12 transition bg-gray-600 hover:-translate-y-1 hover:scale-110 hover:bg-gray-600 duration-150">
                         Regresar
                     </button>
                         <button className="font-serif rounded  w-32 h-12  transition bg-gray-600 hover:-translate-y-1 hover:scale-110 hover:bg-gray-600 duration-150">
                             Modificar
-                        </button></div>
+                        </button></p>
                 </div>
             </div>
         </div>
