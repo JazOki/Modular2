2ed959a7fe7ead452af6527cf6a72544f8ac8469
Tamagochi02 <mirytamara@gmail.com>
2022-05-05 13:58:15 -0500

    little changes in parameters

--- components/SidebarButton.jsx
+++ components/SidebarButton.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-const SidebarButton = ({ nombre, icono, href }) => {
+const SidebarButton = ({ nombre, icono, href}) => {
   return (
     <Link href={href || "/dashboard"}>
       <a className="hover:bg-blue-400 w-full flex items-center px-5 h-8">
