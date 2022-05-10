c15b8f25a76434391c6acf7d089b11cf46597b06
Tamagochi02 <mirytamara@gmail.com>
2022-05-10 12:27:20 -0500

    fetch get

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,11 +4,26 @@ import Link from "next/link";
 import Script from "next/script";
 import { privatePage } from "../lib/ironSessionConfig";
 
-const Layout = ({ children, nombre, matricula, codigo }) => {
-  return (
-    <>
-      <Script id="load-watson" dangerouslySetInnerHTML={{
-        __html: `
+const Layout = ({ children, nombre, matricula, codigo, proyectoE1Id, proyectoE2Id, proyectoE3Id}) => {
+  /*const proyecto = () => {
+    var url = '/api/getproyectobyalumnoid';
+    var data = { matricula: matricula};
+
+    fetch(url, {
+      method: 'GET',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+      .then(data => console.log(data))
+      .catch (error => console.error('Error:', error))
+  }*/
+
+return (
+  <>
+    <Script id="load-watson" dangerouslySetInnerHTML={{
+      __html: `
         function preSendhandler(event) {
           event.data.context.skills['main skill'].user_defined.ismember = true;    
           event.data.context.skills['main skill'].user_defined.username = '${nombre}';
@@ -24,55 +39,55 @@ const Layout = ({ children, nombre, matricula, codigo }) => {
               instance.render(); 
             }
           };`,
-      }}
-      />
-      <Script src="https://web-chat.global.assistant.watson.appdomain.cloud/loadWatsonAssistantChat.js" />
-      <Script src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/latest/WatsonAssistantChatEntry.js" />
-      <div className="flex flex-col w-screen h-screen">
-        <header className="px-5 bg-blue-900 text-right h-12 flex items-center justify-between">
-          <p className="ml-5 text-white font-serif flex text-center items-start px-14">
-            Alumno
-          </p>
-          <p className="flex">
-            <span className="text-white mr-2 material-icons">
-              account_circle{" "}
-            </span>
-            <span className="text-white font-serif  flex items-center">
-              {" "}
-              {nombre}
-            </span>
-          </p>
-        </header>
+    }}
+    />
+    <Script src="https://web-chat.global.assistant.watson.appdomain.cloud/loadWatsonAssistantChat.js" />
+    <Script src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/latest/WatsonAssistantChatEntry.js" />
+    <div className="flex flex-col w-screen h-screen">
+      <header className="px-5 bg-blue-900 text-right h-12 flex items-center justify-between">
+        <p className="ml-5 text-white font-serif flex text-center items-start px-14">
+          Alumno
+        </p>
+        <p className="flex">
+          <span className="text-white mr-2 material-icons">
+            account_circle{" "}
+          </span>
+          <span className="text-white font-serif  flex items-center">
+            {" "}
+            {nombre}
+          </span>
+        </p>
+      </header>
 
-        <div className="flex flex-grow">
-          <aside className="w-64 flex flex-col justify-evenly bg-gray-800">
-            <div className="flex flex-col items-center">
-              <span className="text-white flex justify-center material-icons text-9xl">
-                account_circle
-              </span>
-            </div>
-            <div>
-              <p className="text-blue-500 justify-center border-b border-blue-500 flex items-center">
-                Documentación
-              </p>
-              <SidebarButton href="/dashboard_registro" nombre="Registrar" icono="description" />
-              <SidebarButton href="/dashboard_modificaciones" nombre="Modificaciones" icono="note_alt" />
-              <SidebarButton href="/dashboard_consultas" nombre="Consultas" icono="source" />
-              <SidebarButton nombre="Observaciones" icono="find_in_page" />
-            </div>
-            <div>
-              <p className="text-blue-500 justify-center border-b border-blue-500 flex items-center">
-                Configuraciones
-              </p>
-              <SidebarButton nombre="Perfil" icono="person" />
-              <SidebarButton href="/api/logout/index" nombre="Salir" icono="exit_to_app"/>
-            </div>
-          </aside>
-          <div className="flex-grow ">{children}</div>
-        </div>
+      <div className="flex flex-grow">
+        <aside className="w-64 flex flex-col justify-evenly bg-gray-800">
+          <div className="flex flex-col items-center">
+            <span className="text-white flex justify-center material-icons text-9xl">
+              account_circle
+            </span>
+          </div>
+          <div>
+            <p className="text-blue-500 justify-center border-b border-blue-500 flex items-center">
+              Documentación
+            </p>
+            <SidebarButton href="/dashboard_registro" nombre="Registrar" icono="description" />
+            <SidebarButton href="/dashboard_modificaciones" nombre="Modificaciones" icono="note_alt" />
+            <SidebarButton href="/dashboard_consultas" nombre="Consultas" icono="source" />
+            <SidebarButton nombre="Observaciones" icono="find_in_page" />
+          </div>
+          <div>
+            <p className="text-blue-500 justify-center border-b border-blue-500 flex items-center">
+              Configuraciones
+            </p>
+            <SidebarButton nombre="Perfil" icono="person" />
+            <SidebarButton href="/api/logout" nombre="Salir" icono="exit_to_app" />
+          </div>
+        </aside>
+        <div className="flex-grow ">{children}</div>
       </div>
-    </>
-  );
+    </div>
+  </>
+);
 };
 
 // rutas protegidas
