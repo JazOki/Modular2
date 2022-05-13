bff2ee0b1b34d68737d7bceeaee339a8ee31470a
Tamagochi02 <mirytamara@gmail.com>
2022-05-13 15:31:54 -0500

    api to get all information of alumno

--- pages/api/getproyectobyalumnoid.js
+++ pages/api/getproyectobyalumnoid.js
@@ -1,15 +1,37 @@
 import { prisma } from "../../lib/prisma";
 
 export default async function (req, res){
-    if (req.method != 'GET') return res.status(400).send('Metodo no soportado')
+    //if (req.method != 'GET') return res.status(400).send('Metodo no soportado')
     const {
         matricula
     } = req.query
     if (!matricula) return res.status(400).send('')
+    // const alumno = await prisma.Alumno.findUnique({
+    //     where: {
+    //         matricula
+    //     }
+    // })
     const alumno = await prisma.Alumno.findUnique({
         where: {
             matricula
+        },
+        include:{
+            proyectoE1: {
+                include:{
+                    docente:true
+                }
+            },
+            ProyectoE2: {
+                include:{
+                    docente:true
+                }
+            },
+            ProyectoE3: {
+                include:{
+                    docente:true
+                }
+            }
         }
     })
-    res.json({proyectoE1Id:alumno.proyectoE1Id})
+    res.json({Alumno:alumno})
 }
