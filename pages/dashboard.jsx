00675c3073a9aae65ce487280df8c32f7560c424
andizki <andy190495mrtz@gmail.com>
2022-05-18 08:45:38 -0500

    add "protected routes"

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,28 +1,45 @@
 import Layout from "../components/Layout";
 import { privatePage } from "../lib/ironSessionConfig";
 
-const dashboard = ({nombre, isEnable, codigo, matricula}) => {
-  return < Layout nombre={nombre} matricula={matricula} estado={isEnable?"activo":"inactivo"} codigo={codigo} >
-    
+const dashboard = ({ alumno, nombre, isEnable, codigo, matricula }) => {
+  return < Layout alumno={alumno} nombre={nombre} matricula={matricula} estado={isEnable ? "activo" : "inactivo"} codigo={codigo} >
+
   </Layout>
 };
+// rutas protegidas
+export const getServerSideProps = privatePage(async (context) => {
+  const user = context.req.session.user; //si no hay un usuario
+  if (!user) {
+    return {
+      redirect: {
+        destination: "/api/logout",
+        permanent: false,
+      },
+    };
+  }
 
-  // rutas protegidas
-  export const getServerSideProps = privatePage((context) => {
-    const user = context.req.session.user; //si no hay un usuario
-    if (!user) {
-          return {
-            redirect: {
-              destination: "/api/logout",
-              permanent: false,
-            },
-          };
-    }
+  try {
+    var url = `http://localhost:3000/api/getproyectobyalumnoid?matricula=${user.matricula}`;
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const {alumno} = await response.json()
+  return {
+    props: {
+      ...user,
+      alumno
+    },
+  };
+} catch (error) {
     return {
       props: {
         ...user
       },
     };
-  });
+  }
+});
 
 export default dashboard;
