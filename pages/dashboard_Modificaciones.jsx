b3083f9dd2f9fa8bf3f62a6bdf3d78a66c4de82c
JazOki <castellanossilvia537@gmail.com>
2022-09-22 09:14:00 -0500

    edit container

--- pages/dashboard_Modificaciones.jsx
+++ pages/dashboard_Modificaciones.jsx
@@ -3,47 +3,42 @@ import Layout from "../components/Layout";
 const Modificaciones = () => {
   return (
     <Layout>
-      {/* bg-gray-600 */}
-      <div className=" flex flex-col h-full font-serif text-left text-xl bg-white">
-        <form className="m-auto">
-          <div className=" ml-80 pl-80 font-serif text-xl text-black-600">
-            <p className="  ">
-              Etapas:
-              <select
-                name="select"
-                className="font-serif ml-2 rounded-full bg-blue text-black-600 w-auto outline-double outline-3 outline-offset-2 ..."
-              >
-                <option value="etapa1">Etapa 1: Planteamiento del problema </option>
-                <option value="etapa2">Etapa 2: Resumen del protyecto </option>
-                <option value="etapa3">Etapa 3: Documentación final </option>
-              </select>
-            </p>
-          </div>
-          <div>
-            <p className="font-serif text-xl pt-20 text-black-600 ">
-              Escribe el nombre del proyecto:
-              <input
-                type="text"
-                className="ml-2 w-80 text-black-600 rounded-full outline-double outline-3 outline-offset-2 ..."
-              ></input>
-            </p>
-            <p className="font-serif  text-left text-xl pl-0 pt-5  text-black-600">
-              Elige el archivo PDF que deseas subir:
-              <input
-                type="file"
-                className="ml-2 w-62 text-black-600 rounded-full outline-double outline-3 outline-offset-2 ..."
-              ></input>{" "}
-
-            </p>
-          </div>
-          <div className="ml-96">
-            <p className="pt-7 ml-96 text-white">
-              <button className="font-serif rounded left ml-10 w-32 h-12 transition bg-blue-900 hover:-translate-y-1 hover:scale-110 hover:bg-blue duration-150">
-                Modificar
-              </button>
-            </p>
-          </div>
-        </form>
+      <div className="flex items-center justify-center h-full font-serif text-left text-xl bg-slate-50">
+        <div className="bg-slate-300 rounded shadow-xl">
+          <form>
+            <div class="flex flex-col gap-5 p-6 md:p-8 text-center md:text-left">
+              <div className="w-full flex flex-col justify-end">
+                <p className="font-serif text-xl text-black-600 ">
+                  Etapas:
+                </p>
+                <select
+                  name="select"
+                  className="font-serif text-black-600 w-auto outline-double outline-3"
+                >
+                  <option value="etapa1">Etapa 1: Planteamiento del problema </option>
+                  <option value="etapa2">Etapa 2: Resumen del protyecto </option>
+                  <option value="etapa3">Etapa 3: Documentación final </option>
+                </select>
+              </div>
+              <p className="font-serif text-xl text-black ">
+                Escribe el nombre del proyecto:
+                <input
+                  type="text"
+                  className="text-black outline-double outline-3 w-full"
+                ></input>
+              </p>
+              <p className="font-serif text-xl text-black">
+                Elige el archivo PDF que deseas subir:
+              </p>
+              <input type="file" className=" text-black outline-double outline-3"></input>
+              <div className="flex justify-end mt-3">
+                <button className="text-white font-serif rounded left ml-10 w-32 h-12 transition bg-blue-900 hover:-translate-y-1 hover:scale-110 hover:bg-blue duration-150">
+                  Modificar
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </Layout>
   );
