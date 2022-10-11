e603625ad8bef2c1b70e6a5553fa962fa906ce75
Tamagochi02 <mirytamara@gmail.com>
2022-10-11 07:53:16 -0500

    validation

--- pages/api/getdocentebyid.js
+++ pages/api/getdocentebyid.js
@@ -1,11 +1,12 @@
 import { prisma } from "../../lib/prisma";
 
 export default async function (req, res) {
-    if (req != 'GET') return res.status(400).send('Metodo no soportado')
+    // if (req != 'GET') return res.status(400).send('Metodo no soportado')
     const {
         codigo
     } = req.query
-    if (!id) return res.status(400).send('')
+    if (!codigo) return res.status(400).send('')
+    
     const docente = await prisma.Docente.findUnique({
         where: {
             codigo
